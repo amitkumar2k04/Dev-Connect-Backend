db3650fe287028065d889b85b78610ca20e2e875
amitkumar2k04 <amitkumar2k00@gmail.com>
2025-03-18 07:38:17 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const app = express();
+
+// handling request
+// Handling different request in different way 
+app.use("/",(req, res) => {
+    res.send("Nameste from the Dashboard!! ");
+});
+
+app.use("/hello",(req, res) => {
+    res.send("Hello there!! ");
+});
+
+app.use("/test",(req, res) => {
+    res.send("Hello from the test!! ");
+});
+
+
+// server running on port 3000
+app.listen(3000, () => {
+    console.log("server is sucessfully listning on port 3000 .. ");
+});
+
+
+/*
+Steps performed :
+    - Installed express
+    - create a server
+    - Listen to port 3000
+    - write request handlers for /test, /hello
+    - Installed nodemon and update script inside package.json 
+
+*/
